d395d82c081364e8d63139f90958b916fa1948f8
Roman Strynzha <pamaxas3@gmail.com>
2022-12-15 21:50:17 +0200

    Install storybook

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { App } from '../App';
+
+export default {
+  title: 'Example/App',
+  component: App,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = () => <App />;
+
+export const Primary = Template.bind({});
